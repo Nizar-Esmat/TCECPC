eaf5fbd5c1783eb791c50b21723b62067e302cd4
Nizar Esmat <nizaresmat2000@gmail.com>
2026-08-09 12:39:10 +0300

    feat: update greeting message and add initial package.json and package-lock.json chore: remove outdated e2e tests and jest configuration

--- BackENd/src/app.service.ts
+++ BackENd/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'welcome To Movment control ECPC 2026 system';
   }
 }
